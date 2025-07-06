fc8f7e58c22b697a89dc54d74994b1747c61d58f
codewithabhi8 <codewithabhi8@gmail.com>
2025-07-06 08:08:00 +0530

    Java FX Init

---
+++ src/JavaFXSetup/JavaFXDemo.java
@@ -0,0 +1,16 @@
+package JavaFXSetup;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class JavaFXDemo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
